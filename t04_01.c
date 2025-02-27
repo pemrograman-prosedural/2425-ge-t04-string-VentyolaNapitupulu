d1b5c670e5626e5affc2022edf7e532757eaf799
VentyolaNapitupulu <ventyolanapitupuluh@gmail.com>
2025-02-27 14:18:49 +0700

    Pair 15

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,25 @@
-// NIM - Name
-// NIM - Name
+// 12S24039 - Jody Alfonso Siahaan
+// 12S24042 - Ventyola Rohati Napitupulu
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+    char str[61];
+    char pesan[21];
+    int u;
 
-  return 0;
+    scanf("%60s", str);
+
+    for (u = 0; u < strlen(str); u += 3) {
+        int ascii_code = atoi(strncpy((char[4]) {0}, str + u, 3));
+        pesan[u / 3] = (char) ascii_code;
+    }
+
+    pesan[u / 3] = '\0';
+
+    printf("%s\n", pesan);
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,23 @@
-// NIM - Name
-// NIM - Name
+// 12S24039 - Jody Alfonso Siahaan
+// 12S24042 - Ventyola Rohati Napitupulu
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+    char str[50];
+    int u;
 
-  return 0;
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = 0;
+
+    for (u = 0; u < strlen(str); u ++) {
+        printf("%03d", str[u]);
+        
+    }
+
+    printf("\n");
+
+    return 0;
 }
